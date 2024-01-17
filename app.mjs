181a1bbb060c6fd6b64c2c3095f8938dc689122a
julie reksten <juliereksten@hotmail.com>
2024-01-17 16:39:19 +0100

    updated app.mjs file

--- app.mjs
+++ app.mjs
@@ -6,13 +6,12 @@ let colorsArray = [chalk.blue, chalk.magenta, chalk.yellow, chalk.red, chalk.gre
 let joke = "Why did the strawberry cry? \n It found itself in a jam."
 
 let words = joke.split(" ");
+let coloredJoke = "";
 
 for (let i = 0; i < words.length; i++) {
     let word = words[i];
     let randomColor = getRandomColor(colorsArray);
     let coloredWord = randomColor(word);
-
-    let coloredJoke = "";
     coloredJoke += coloredWord + " ";
 }
 
